a04de4ec9ee955f2abfbc9e561c8a3bf90565a49
jonatasrs <jonatasrs@users.noreply.github.com>
2017-10-23 16:23:20 -0200

    Ajuste em core.d_start.RedirecionarPagina.

--- cs_modules/core.d_start.RedirecionarPagina.js
+++ cs_modules/core.d_start.RedirecionarPagina.js
@@ -3,23 +3,29 @@ function RedirecionarPagina(BaseName, num = 0) {
   /** inicialização do módulo */
   var mconsole = new __mconsole(BaseName + ".RedirecionarPagina");
 
-  mconsole.log("Tentativa " + num);
+  RPagina(BaseName, num);
 
-  var menu = document.getElementById("main-menu");
-  if (menu != null) {
-    var links = menu.getElementsByTagName("a");
-    for (var index in links) {
-      if (links.hasOwnProperty(index)) {
-        var element = links[index];
+  function RPagina(BaseName, num) {
+    mconsole.log("Redirecionamento tentativa: " + num);
 
-        if (element.getAttribute("href").indexOf("controlador.php?acao=procedimento_controlar") != -1) {
-          window.location.assign(element.getAttribute("href"));
+    var menu = document.getElementById("main-menu");
+
+    if (menu != null) {
+      var links = menu.getElementsByTagName("a");
+      for (var index in links) {
+        if (links.hasOwnProperty(index)) {
+          var element = links[index];
+
+          if (element.getAttribute("href").indexOf("controlador.php?acao=procedimento_controlar") != -1) {
+            window.location.assign(element.getAttribute("href"));
+          }
         }
       }
+    } else if (num < 30) {
+      setTimeout(function () {
+        RPagina(BaseName, num + 1);
+      }, 100);
     }
-  } else if (num < 6) {
-    setTimeout(function () {
-      RedirecionarPagina(BaseName, num + 1);
-    }, 100);
   }
+
 }
