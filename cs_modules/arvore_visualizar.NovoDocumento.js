deee80eada6ba50d0fb7edf96cb5b6b3db493148
jonatasrs <jonatasrs@users.noreply.github.com>
2017-11-07 09:05:37 -0200

    Versão beta 14 * Atualização e correção da rotina de incluir o botão NovoDocumento.

--- cs_modules/arvore_visualizar.NovoDocumento.js
+++ cs_modules/arvore_visualizar.NovoDocumento.js
@@ -2,24 +2,37 @@ function NovoDocumento(BaseName) {
   /** inicialização do módulo */
   var mconsole = new __mconsole(BaseName + ".NovoDocumento");
 
-  /* Pega a url de inclusão de documento */
+  /* Verifica se já tem o botão de novo documento */
   var havenewdoc = $('#divArvoreAcoes a[href*="acao=documento_escolher_tipo"]').length;
-  if (havenewdoc < 1) {
-    var head =$(parent.document.getElementById('ifrArvore').contentWindow.document.head).html()
-    var a = head.indexOf("controlador.php?acao=documento_escolher_tipo");
-    var b = 0;
-    var url = "";
+  var havenewdocp = $('#divArvoreAcoes a[href*="acao=documento_receber"]').length;
 
-    /*** Adiciona item novo documento */
-    if (a != -1) {
-      b = head.indexOf("\"", a);
-      url = head.substring(a, b);
-      $("#divArvoreAcoes").prepend("<a id='seipp_newdoc' tabindex='451' class='botaoSEI'><img class='infraCorBarraSistema' src='imagens/sei_incluir_documento.gif' alt='Incluir Documento' title='Incluir Documento'></a>");
-      $("#seipp_newdoc").attr("href", url);
-      mconsole.log("Link adicionado com sucesso.");
-    } else {
-      mconsole.log("Link não encontrado.");
+  /* Se não tiver o botão faz */
+  if (!havenewdoc && !havenewdocp) {
+    /* Pega o head do html */
+    var b = 0;
+    var botao = "";
+    var head =$(parent.document.getElementById('ifrArvore').contentWindow.document.head).html();
+    var tipo = 'documento_escolher_tipo';
+    var a = head.indexOf('<a href="controlador.php?acao=' + tipo);
+
+    if (a == -1) {
+      tipo = 'documento_receber';
+      a = head.indexOf('<a href="controlador.php?acao=' + tipo);
+      if (a == -1) {
+        mconsole.log("Link não encontrado.");
+        return;
+      }
     }
+    mconsole.log(tipo);
+
+    /* Adiciona botão novo documento */
+    b = head.indexOf("</a>", a) + 4;
+    botao = head.substring(a, b);
+    $("#divArvoreAcoes").prepend($(botao));
+    //$("#seipp_newdoc").attr("href", botao);
+    mconsole.log("Link adicionado com sucesso.");
+  } else {
+    mconsole.log("Já existe o botão.")
   }
 }
 
