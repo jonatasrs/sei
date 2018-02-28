67c7f5778989f40d90fe74f18cd714f3b9d001ad
hebert <hebertmm@gmail.com>
2018-02-28 15:50:38 -0300

    Correção do autopreenchimento do andamento no Chrome ver. 64 (issue #44)

--- cs_modules/procedimento_visualizar.AutopreencherAndamento.js
+++ cs_modules/procedimento_visualizar.AutopreencherAndamento.js
@@ -1,20 +1,16 @@
 function AutopreencherAndamento(BaseName) {
   /** inicialização do módulo ***************************************************/
   var mconsole = new __mconsole(BaseName + ".AutopreencherAndamento");
-
-  verificaArvore();
+	setTimeout(verificaArvore, 500);
+    
 
   /****Auto preenchimento do andamento*******************************************/
   function enviarOficio(event){
     var textoPadrao = "Solicita-se ao protocolo a expedição do %nome (SEI nº %num), por meio de Correspondência Simples Nacional com Aviso de Recebimento.";
-    //$("#ifrVisualizacao").contents().find("#txaDescricao").val(textoPadrao.replace('%nome', event.data.name).replace('%num', event.data.sei));
     $(parent.document.getElementById('ifrVisualizacao')).contents().find("#txaDescricao").val(textoPadrao.replace('%nome', event.data.name).replace('%num', event.data.sei));
   }
 
   function criaLink(i, e){
-    var a = document.createElement("span");
-    a.innerHTML = "C";
-    a.setAttribute("id","teste");
     var link = $('<a><img src="'+browser.extension.getURL("icons/ect.png")+'" title="Preencher atualização de andamento (abra a tela de atualizar andamento antes de clicar!)"> </img></a>');
     var sp = $(this).find("span");
     var text = sp.text();
@@ -35,10 +31,6 @@ function AutopreencherAndamento(BaseName) {
 
   }
   function verificaArvore(){
-    //var a = $("#ifrArvore").contents().find("div.infraArvore").filter(":last");
-    //var b = a.find("a[target='ifrVisualizacao']");
-    var b = $("div[class='infraArvore']:last a[target='ifrVisualizacao']");
-
-    b.each(criaLink);
+    $("div[class='infraArvore']:last a[target='ifrVisualizacao']").each(criaLink);
   }
 }
