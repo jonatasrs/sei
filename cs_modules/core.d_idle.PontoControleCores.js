75734d6ec152771bbaf5a0e36f5462b5d7544f9c
jonatasrs <jonatasrs@users.noreply.github.com>
2018-03-02 15:33:01 -0300

    Melhorias no PontoControleCores.

--- cs_modules/core.d_idle.PontoControleCores.js
+++ cs_modules/core.d_idle.PontoControleCores.js
@@ -1,7 +1,21 @@
-	/* (ANATEL) Anatel Ponto de controle cores */
 function PontoControleCores(BaseName) {
   /** inicialização do módulo */
   var mconsole = new __mconsole(BaseName + ".PontoControleCores");
+  var AplicarNestasPaginas = [
+    "procedimento_visualizar",
+    "procedimento_controlar"
+  ];
 
-  AdicionarLinkCss(document, "seipp-pontocores-anatel", "cs_modules/themes/PontoControleCores_Anatel.css");
+  if (AplicarLinkCss()) {
+    mconsole.log(document.baseURI);
+    AdicionarLinkCss(document, "seipp-pontocores-anatel", "cs_modules/themes/PontoControleCores_Anatel.css");
+  }
+
+  function AplicarLinkCss() {
+    var Aplicar = false;
+    AplicarNestasPaginas.forEach(function (item) {
+      if (document.baseURI.indexOf("acao=" + item) != -1) { Aplicar = true; }
+    });
+    return Aplicar;
+  }
 }
