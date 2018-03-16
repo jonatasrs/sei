13e3eae221c1ccfe429fce44010cb40c21a69b76
Juliano Fischer Naves <julianofischer@gmail.com>
2018-03-16 19:55:21 -0400

    corrigindo typos - versão em funcionamento

--- cs_modules/core.d_idle.ExibirNomeDoProcesso.js
+++ cs_modules/core.d_idle.ExibirNomeDoProcesso.js
@@ -1,12 +1,14 @@
 function ExibirNomeDoProcesso(BaseName) {
   /** inicialização do módulo */
-  var mconsole = new __mconsole(BaseName + ".ExibirNomeDoProcesso");
+  //mconsole.log("entrou1");
+  //var mconsole = new __mconsole(BaseName + ".ExibirNomeDoProcesso");
+  //mconsole.log("entrou2");
 
   var processos = document.getElementsByClassName("processoVisualizado");
   for (var i = 0; i < processos.length; i++) {
     var p = processos[i];
     var text = p.onmouseover+"";
-    var extracted = text.substring(text.lastIndexOf("(")+1,text.lastIndexOf(")"$
+    var extracted = text.substring(text.lastIndexOf("(")+1,text.lastIndexOf(")"));
     extracted = extracted.substring(1, extracted.lastIndexOf(",")-1);
   
     if (extracted.length>0){
