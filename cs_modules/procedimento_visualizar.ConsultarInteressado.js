92f34942edb012116fd09b5c04d747205b01cb88
jonatasrs <jonatas.eletrica@gmail.com>
2018-04-06 17:15:07 -0300

    Pequenas correções

--- cs_modules/procedimento_visualizar.ConsultarInteressado.js
+++ cs_modules/procedimento_visualizar.ConsultarInteressado.js
@@ -3,12 +3,12 @@ function ConsultarInteressado(BaseName) {
   var mconsole = new __mconsole(BaseName + ".ConsultarInteressado");
 
   /** Variaveis *****************************************************************/
-  var processo = {numero: "",interessados: [], tipo: ""};
+  var processo = { numero: "", interessados: [], tipo: "" };
 
   /** Pega a url de alteração do processo ***************************************/
   var head = $('head').html();
   var a = head.indexOf("controlador.php?acao=procedimento_alterar&");
-  if (a == -1) { a = head.indexOf("controlador.php?acao=procedimento_consultar&");}
+  if (a == -1) { a = head.indexOf("controlador.php?acao=procedimento_consultar&"); }
   if (a == -1) {
     mconsole.log("Consulta não disponível para este processo!!!");
     return;
@@ -25,7 +25,7 @@ function ConsultarInteressado(BaseName) {
     processo.numero = $("#divArvore > a > span[id^='span']").text().replace(/\D/g, '');
     mconsole.log("Lendo dados do processo: " + processo.numero);
     processo.tipo = $html.find("#selTipoProcedimento option[selected='selected']").text();
-   processo.interessados = $html.find("#selInteressadosProcedimento option").map(function () {  return $(this).text();}).get();
+    processo.interessados = $html.find("#selInteressadosProcedimento option").map(function () { return $(this).text(); }).get();
 
     DetalheProcesso_Criar();
     DetalheProcesso_Preencher();
@@ -49,13 +49,13 @@ function ConsultarInteressado(BaseName) {
   }
 
   function ExibirDadosProcesso($html) {
-    setTimeout(function(){
+    setTimeout(function () {
       // https://stackoverflow.com/questions/726816/how-to-access-iframe-parent-page-using-jquery/726866
       // https://stackoverflow.com/questions/6316979/selecting-an-element-in-iframe-jquery
       var iframe = window.parent.document.getElementById('ifrVisualizacao');
       var $iframe = $(iframe);
       var mask_processo = $("#divArvore > a > span[id^='span']").text();
-      var interessados = $html.find("#selInteressadosProcedimento option").map(function() {return $(this).text();}).get();
+      var interessados = $html.find("#selInteressadosProcedimento option").map(function () { return $(this).text(); }).get();
       var descricao = $html.find("#txtDescricao").val();
       var data = $html.find("#txtDtaGeracaoExibir").val();
       mconsole.log(mask_processo);
@@ -63,15 +63,15 @@ function ConsultarInteressado(BaseName) {
       mconsole.log(descricao);
       mconsole.log(data);
 
-      $iframe.contents().find("#divInformacao").css('width','300px');
+      $iframe.contents().find("#divInformacao").css('width', '300px');
       mconsole.log($iframe.prop("id"));
 
       $("<div id='detalhes' style='margin-left: 300px; border: 1px solid; padding: 2px;'/>")
         .insertAfter($iframe.contents().find("#divInformacao"))
         .append('<div id="divInfraBarraLocalizacao" class="infraBarraLocalizacao" style="display:block;">Dados do Processo</div>')
-        .append('<div id="divProtocoloExibir" class="infraAreaDados" style="height:4.5em; clear: both;"><label id="lblProtocoloExibir" for="txtProtocoloExibir" accesskey="" class="infraLabelObrigatorio">Protocolo:</label><input id="txtProtocoloExibir" name="txtProtocoloExibir" class="infraText infraReadOnly" readonly="readonly" type="text" value="' + mask_processo + '""><label id="lblDtaGeracaoExibir" for="txtDtaGeracaoExibir" accesskey="" class="infraLabelObrigatorio" style="margin-left: 20px;">Data de Autuação:</label><input type="text" id="txtDtaGeracaoExibir" name="txtDtaGeracaoExibir" class="infraText infraReadOnly" readonly="readonly" /></div>')
+        .append('<div id="divProtocoloExibir" class="infraAreaDados" style="height:4.5em; clear: both;"><label id="lblProtocoloExibir" for="txtProtocoloExibir" accesskey="" class="infraLabelObrigatorio">Protocolo:</label><input id="txtProtocoloExibir" name="txtProtocoloExibir" class="infraText infraReadOnly" readonly="readonly" type="text" value="' + mask_processo + '"><label id="lblDtaGeracaoExibir" for="txtDtaGeracaoExibir" accesskey="" class="infraLabelObrigatorio" style="margin-left: 20px;">Data de Autuação:</label><input type="text" id="txtDtaGeracaoExibir" name="txtDtaGeracaoExibir" class="infraText infraReadOnly" readonly="readonly" /></div>')
         .append('<div id="divTipoProcedimento" class="infraAreaDados" style="height:4.5em; clear: none;"><label id="lblTipoProcedimento" for="selTipoProcedimento" accesskey="" class="infraLabelObrigatorio">Tipo do Processo:</label><input id="selTipoProcedimento" name="selTipoProcedimento" class="infraText infraReadOnly" readonly="readonly" style="width: 95%;" value="' + processo.tipo + '"></div>')
-        .append('<div id="divDescricao" class="infraAreaDados" style="height:4.7em; clear: none;"><label id="lblDescricao" for="txtDescricao" accesskey="" class="infraLabelOpcional">Especificação:</label><input id="txtDescricao" name="txtDescricao" class="infraText infraReadOnly" type="text" style="width: 95%;"></div>')
+        .append('<div id="divDescricao" class="infraAreaDados" style="height:4.7em; clear: none;"><label id="lblDescricao" for="txtDescricao" accesskey="" class="infraLabelOpcional">Especificação:</label><input id="txtDescricao" name="txtDescricao" class="infraText infraReadOnly" readonly="readonly" type="text" style="width: 95%;"></div>')
         .append('<div id="divInteressados" class="infraAreaDados" style="height:11em; clear: none;"><label id="lblInteressadosProcedimento" for="txtInteressadoProcedimento" accesskey="I" class="infraLabelOpcional"><span class="infraTeclaAtalho">I</span>nteressados:</label><br/><textarea id="txtInteressadosProcedimento" name="txtInteressadosProcedimento" class="infraText infraReadOnly" readonly="readonly" style="width: 95%";>' + interessados.join("\n") + '</textarea></div>');
 
       var newiframe = window.parent.document.getElementById('ifrVisualizacao');
