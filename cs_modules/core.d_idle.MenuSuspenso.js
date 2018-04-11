bff3a5f206f17a9d075873190456f1772501a9f1
jonatasrs <jonatas.eletrica@gmail.com>
2018-04-11 08:22:42 -0300

    Melhorias de performance no Menu Suspenso.

--- cs_modules/core.d_idle.MenuSuspenso.js
+++ cs_modules/core.d_idle.MenuSuspenso.js
@@ -3,11 +3,14 @@ function MenuSuspenso(BaseName) {
   /** inicialização do módulo */
   var mconsole = new __mconsole(BaseName + ".MenuSuspenso");
 
+  if ($("body").attr("onload").indexOf("infraMenuSistemaEsquema") != -1){
+      $("body").attr("onload", $("body").attr("onload") + "infraOcultarMenuSistemaEsquema();");
+  }
   if (!$("#main-menu").length) return;
   $("#main-menu, #divInfraAreaTelaE").hide();
 
   if ($("#lnkInfraMenuSistema").attr("title").indexOf("Ocultar") != -1) {
-		$("#divInfraAreaTelaD").width("99%");
+    $("#divInfraAreaTelaD").width("99%");
   }
   /* Oculta o botao de exibir menu */
   $("#lnkInfraMenuSistema").hide();
@@ -15,29 +18,30 @@ function MenuSuspenso(BaseName) {
   $("#divInfraAreaTelaE > div > img").attr("title", $("#divInfraAreaTelaE > div > p").text());
   $("#divInfraAreaTelaE > div > div").hide();
 
-	$("#divInfraAreaTelaE").css({
-		"position": "absolute",
-		"display": "block",
+  $("#divInfraAreaTelaE").css({
+    "position": "absolute",
+    "display": "block",
     "width": "auto",
-    "background-color" : "#d7d7d7"
+    "background-color": "#d7d7d7"
   });
 
-	$("#divInfraAreaTelaE > div").css({"border-bottom": "5px solid"});
+  $("#divInfraAreaTelaE > div").css({ "border-bottom": "5px solid" }).hide();
 
-	$("#divInfraBarraSistemaE img").click(function(e){
+  $("#divInfraBarraSistemaE img").click(function (e) {
     e.stopPropagation(); /* impede a propagação do evento click */
     if ($("#main-menu").attr("style") != undefined) {
       $("#main-menu").removeAttr("style");
       $("#divInfraAreaTelaE").hide();
     }
     $("#divInfraAreaTelaE").toggle("fast");
+    $("#divInfraAreaTelaE > div").show();
   });
 
-	$("#divInfraAreaTelaE").addClass("seipp-menu");
-	$("#main-menu ul").addClass("seipp-menu");
-	$("#divInfraAreaTelaE *").click(function(e){e.stopPropagation();})
-	/* Oculta o menu ao clicar fora */
-	$('body').click(function(){
-		if (!$("#divInfraAreaTelaE").is(':hidden')){$("#divInfraAreaTelaE").toggle("fast");}
+  $("#divInfraAreaTelaE").addClass("seipp-menu");
+  $("#main-menu ul").addClass("seipp-menu");
+  $("#divInfraAreaTelaE *").click(function (e) { e.stopPropagation(); })
+  /* Oculta o menu ao clicar fora */
+  $('body').click(function () {
+    if (!$("#divInfraAreaTelaE").is(':hidden')) { $("#divInfraAreaTelaE").toggle("fast"); }
   });
 }
