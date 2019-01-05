d93263b12e5467e74c4a90c21e26d2c7b7a94e9e
luis.reischak <luis.reischak@gmail.com>
2019-01-05 14:06:39 -0200

    Corrigidos os bugs de incompatibilidade com a versão 71 do Chrome

--- cs_modules/procedimento_controlar.js
+++ cs_modules/procedimento_controlar.js
@@ -24,7 +24,19 @@ if (ModuleInit(BaseName, true)) {
         break;
     }
   }, this);
-  AdicionarOrdenacao(BaseName);
+
+
+  /* Ao invés de injetar este script, carregá-lo via script tag,
+    para que seja acessível pelas funções cujo contexto é a página, e não a extensão. */
+  runExtensionScript('lib/jquery.tablesorter.min.js');
+  runExtensionScript('lib/jquery.tablesorter.widgets.min.js');
+
+  /* Ao invés de injetar o AdicionarOrdenacao, carregá-lo no contexto da página */
+  runExtensionScript(
+    'cs_modules/procedimento_controlar.AdicionarOrdenacao.js',
+    `AdicionarOrdenacao('${BaseName}');`
+  );
+
   SelecionarMultiplosProcessos(BaseName);
   ConfirmarAntesConcluir(BaseName);
 }
