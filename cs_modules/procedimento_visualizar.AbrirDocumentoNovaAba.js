1d1eb19c0e9b411d3a2aa6f1661ccb6b5e3374ab
luis.reischak <luis.reischak@gmail.com>
2019-01-06 16:32:34 -0200

    fix #95 - execução do código que atribui o script no evento executado no contexto da página

--- cs_modules/procedimento_visualizar.AbrirDocumentoNovaAba.js
+++ cs_modules/procedimento_visualizar.AbrirDocumentoNovaAba.js
@@ -3,11 +3,17 @@ function AbrirDocumentoNovaAba(BaseName) {
   var script = "var id = $(this).attr('id').substr(4); if (event.ctrlKey) { $(this).parent().attr('href', objArvore.getNo(id).src); } else { $(this).parent().attr('href', objArvore.getNo(id).href); }"
 
   function Executar() {
-    $("#divArvore > div a[target='ifrVisualizacao'] > span").each(function () {
-      if ($(this).attr("onclick") == undefined) {
-        $(this).attr("onclick", script);
-      }
-    });
+    
+    execOnPage(`
+
+      $("#divArvore > div a[target='ifrVisualizacao'] > span").each(function () {
+        if ($(this).attr("onclick") == undefined) {
+          $(this).attr("onclick", "${script}");
+        }
+      });
+
+    `);
+
   }
 
   ExecutarNaArvore(mconsole, Executar);
