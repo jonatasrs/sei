6eb09d5a69cdeb28845074e0e562eef5a35344ab
Jonatas Evaristo <jonatasers@PR197812.presidencia.gov.br>
2019-12-06 16:29:07 -0300

    Fix copiar link processo/documento

--- cs_modules/procedimento_visualizar.CopiarLinkInterno.js
+++ cs_modules/procedimento_visualizar.CopiarLinkInterno.js
@@ -1,15 +1,23 @@
-function CopiarLinkInterno(BaseName) {
+function CopiarLinkInterno (BaseName) {
   var mconsole = new __mconsole(BaseName + ".CopiarLinkInterno");
   var linkurl = GetBaseUrl() + "controlador.php?acao=procedimento_trabalhar&id_procedimento=";
   var idmod = "seipp-cli";
 
   ExecutarNaArvore(mconsole, Iniciar);
 
-  function Iniciar(params) {
-    $("#divArvore > a[target='ifrVisualizacao']:has(span)").each(function () {
+  function Iniciar (params) {
+    let idProcesso = null
+    let arvore = $("#divArvore > a[target='ifrVisualizacao']:has(span)")
+    if (!arvore.length) {
+      arvore = $(".infraArvore > a[target='ifrVisualizacao']:has(span)")
+    }
+    arvore.each(function () {
       let id = $(this).find("span").attr("id").substr(4);
+      let hasDocumento = this.href.indexOf('documento') > 0
       let $imgCopyLink = $("<img/>");
       if ($("#" + idmod + id).length != 0) return;
+
+      if (!hasDocumento) { idProcesso = id }
 
       mconsole.log(" + : " + id);
       $imgCopyLink
@@ -19,7 +27,7 @@ function CopiarLinkInterno(BaseName) {
         .on("click", function () {
           var $copy = $("<input>");
           $("body").append($copy);
-          $copy.val(linkurl + id).select();
+          $copy.val(linkurl + (hasDocumento ? idProcesso + '&id_documento=' : '') + id).select();
           try {
             document.execCommand("copy");
             AnimacaoFade(this);
