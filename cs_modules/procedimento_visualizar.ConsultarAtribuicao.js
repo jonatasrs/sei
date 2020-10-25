1f42290ecdc4e1bb6b003af3425e4d326801d8b7
luis.reischak <luis.reischak@gmail.com>
2020-10-25 13:10:27 -0300

    mostra quem possui credenciais de acesso nos processos sigilosos

--- cs_modules/procedimento_visualizar.ConsultarAtribuicao.js
+++ cs_modules/procedimento_visualizar.ConsultarAtribuicao.js
@@ -9,22 +9,40 @@ function ConsultarAtribuicao(BaseName) {
   ConsultarInteressado_Criar(unidadeAtual, dadosAtribuicao);
   
   function ConsultarInteressado_Criar(unidadeAtual, dadosAtribuicao) {
-    let container = $("#container").length > 0 ? $("#container") : $("body");
-    
+
+    const container = $("#container").length > 0 ? $("#container") : $("body");
+ 
     container.append(`
-      <div class='seipp-separador'><span>Atribuído para</span></div>
+      <div class='seipp-separador'><span>${dadosAtribuicao.sigiloso ? "Credencial para" : "Atribuído para"}</span></div>
       <div id='seipp_atribuicao'>
         <p class="seipp-atribuido-para" title=""></p>
       </div>
-    `);  
+    `);
 
-    if (dadosAtribuicao.login) {
-      $('#seipp_atribuicao > p.seipp-atribuido-para').text(dadosAtribuicao.login);
-      $('#seipp_atribuicao > p.seipp-atribuido-para').attr('title', `Atribuído na unidade ${unidadeAtual} para ${dadosAtribuicao.nome} (${dadosAtribuicao.login}).`);
+    if (dadosAtribuicao.usuarios.length === 0) {
+      $('#seipp_atribuicao').append($('<p />', {
+        class: 'seipp-atribuido-para seipp-processo-sem-atribuicao',
+        text: '(processo sem atribuição)',
+      }));
     } else {
-      $('#seipp_atribuicao > p.seipp-atribuido-para').addClass('seipp-processo-sem-atribuicao');
-      $('#seipp_atribuicao > p.seipp-atribuido-para').text('(processo sem atribuição)');
+      dadosAtribuicao.usuarios.forEach(function(usuario) {
+        $('#seipp_atribuicao').append($('<p />', {
+          class: 'seipp-atribuido-para',
+          text: usuario.login,
+          title: dadosAtribuicao.sigiloso
+            ? `Credencial para ${usuario.nome} (${usuario.login}) na unidade ${unidadeAtual}.`
+            : `Atribuído na unidade ${unidadeAtual} para ${usuario.nome} (${usuario.login}).`
+        }));
+      });
+      if (dadosAtribuicao.sigiloso && typeof dadosAtribuicao.mais !== 'undefined' && dadosAtribuicao.mais > 0) {
+        $('#seipp_atribuicao').append($('<p />', {
+          class: 'seipp-atribuido-para seipp-atribuido-para-mais',
+          text: `+${dadosAtribuicao.mais}`,
+          title: `Mais ${dadosAtribuicao.mais} usuário(s) de outra(s) área(s).`,
+        }));        
+      }
     }
+
   }
 
   function obter_UnidadeAtual() {
@@ -37,14 +55,48 @@ function ConsultarAtribuicao(BaseName) {
 
   function obter_Atribuicao(unidade) {
     const ultimaScriptTag = document.getElementsByTagName('script')[document.getElementsByTagName('script').length-1];
-    let regex = new RegExp(String.raw`^Nos\[0\].html = 'Processo aberto.*<a alt=".*" title=".*" class="ancoraSigla">${unidade}<\/a>(.*?)[\.]?<br \/>`, 'm');
-    let resultado = regex.exec(ultimaScriptTag.innerHTML);
-    if (resultado === null || resultado.length !== 2) return null; /* processo não se encontra aberto */
-    let atribuicaoStr = resultado[1];
-    regex = /\(atribuído para <a alt=".*" title="(.*?)" class="ancoraSigla">(.*?)<\/a>\)/m;
-    resultado = regex.exec(atribuicaoStr);
-    if (resultado === null || resultado.length !== 3) return { nome: null, login: null }; /* processo aberto, mas sem atribuição */
-    return { nome: resultado[1], login: resultado[2] };
+    
+    /* verificar se processo está aberto em alguma unidade */
+    if (!/^Nos\[0\].html = 'Processo aberto/m.test(ultimaScriptTag.innerHTML)) return null;
+    
+    /* extrai o que foi atribuído para a variável Nos[0].html */
+    const rUsuarios = /^Nos\[0\].html = '(.*)';/m.exec(ultimaScriptTag.innerHTML);
+    if (!rUsuarios || rUsuarios.length !== 2) return null;
+    const html = rUsuarios[1];
+
+    /* caso de processos públicos ou restritos, busca por atribuição */
+    if (/(Processo aberto nas unidades:|Processo aberto somente na unidade)/m.test(html)) {
+
+      let regex = new RegExp(String.raw`(?<=<a alt=".*" title=".*" class="ancoraSigla">)${unidade}<\/a>(.*?)[\.]?<br \/>`, 'm');
+      let resultado = regex.exec(html);
+      if (resultado === null || resultado.length !== 2) return null; /* processo não está aberto na unidade do usuário */
+      let atribuicaoStr = resultado[1];
+      regex = /\(atribuído para <a alt=".*" title="(.*?)" class="ancoraSigla">(.*?)<\/a>\)/m;
+      resultado = regex.exec(atribuicaoStr);
+      if (resultado === null || resultado.length !== 3) return { sigiloso: false, usuarios: [] }; /* processo está aberto na unidade do usuário, mas sem atribuição */
+      return { sigiloso: false, usuarios: [{ nome: resultado[1], login: resultado[2] }] };
+      
+      /* caso de processos sigilosos, busca pro credenciais */
+    } else if (/(Processo aberto com os usuários:|Processo aberto somente com o usuário)/m.test(html)) {
+      let regex = /(?<=<a alt=".*?" title="(.*?)" class="ancoraSigla">(.*?))(?=<\/a>&nbsp;\/&nbsp;<a alt=".*?" title=".*?" class="ancoraSigla">(.*?)<\/a>)/g;
+      let m;
+      let usuarios = [];
+      let mais = 0;
+      while ((m = regex.exec(html)) !== null) {
+        if (m.index === regex.lastIndex) regex.lastIndex++;
+        let [nome, login, unidade] = [m[1], m[2], m[3]];
+        if (unidade === unidadeAtual) {
+          usuarios.push({ nome: nome, login: login });
+        } else {
+          mais++;
+        }
+      }
+      return { sigiloso: true, usuarios: usuarios, mais: mais };
+
+    /* algum outro texto não identificado */
+    } else {
+      return null;
+    }
   }
    
 }
