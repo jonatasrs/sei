53f079565250e0a9925610b7d74dee86ad0aff2d
Jonatas Evaristo <jonatas.eletrica@gmail.com>
2022-08-22 17:57:11 -0300

    Atualiza make

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -f seipp.zip
-zip -r seipp.zip . -x ".git*" "scripts/*" "node_modules/*"
+zip -r seipp.zip . -x ".git*" "scripts/*" "node_modules/*" "yarn*"
 echo "Create seipp.zip"
