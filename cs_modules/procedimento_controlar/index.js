03cc68aefecf7f84fc8e460835bc076232ee189b
Jonatas Evaristo <jonatas.eletrica@gmail.com>
2022-08-30 15:41:43 -0300

    Desabilita ordenação da tabela na versão 4.0 do SEI

--- cs_modules/procedimento_controlar/index.js
+++ cs_modules/procedimento_controlar/index.js
@@ -31,16 +31,18 @@ if (ModuleInit(BaseName, true)) {
     }
   }, this)
 
-  /* Ao invés de injetar este script, carregá-lo via script tag,
-    para que seja acessível pelas funções cujo contexto é a página, e não a extensão. */
-  addScriptToPage('lib/jquery.tablesorter/jquery.tablesorter.min.js')
-  addScriptToPage('lib/jquery.tablesorter/jquery.tablesorter.widgets.min.js')
+  if (seiVersionCompare('<', '4')) {
+    /* Ao invés de injetar este script, carregá-lo via script tag,
+      para que seja acessível pelas funções cujo contexto é a página, e não a extensão. */
+    addScriptToPage('lib/jquery.tablesorter/jquery.tablesorter.min.js')
+    addScriptToPage('lib/jquery.tablesorter/jquery.tablesorter.widgets.min.js')
 
-  /* Ao invés de injetar o adicionarOrdenacao, carregá-lo no contexto da página */
-  addScriptToPage(
-    'cs_modules/procedimento_controlar/adicionarOrdenacao.js',
-    `adicionarOrdenacao('${BaseName}');`
-  )
+    /* Ao invés de injetar o adicionarOrdenacao, carregá-lo no contexto da página */
+    addScriptToPage(
+      'cs_modules/procedimento_controlar/adicionarOrdenacao.js',
+      `adicionarOrdenacao('${BaseName}');`
+    )
+  }
 
   SelecionarMultiplosProcessos(BaseName)
   confirmarAntesConcluir(BaseName)
