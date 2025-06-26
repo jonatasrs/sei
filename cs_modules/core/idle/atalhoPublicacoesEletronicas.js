a1973fd894d053fa5986f479fdaf114cd4ef0d1c
Jonatas Evaristo <jonatas.eletrica@gmail.com>
2025-06-26 09:13:07 -0300

    Fix: atalhoPublicacoesEletronicas

--- cs_modules/core/idle/atalhoPublicacoesEletronicas.js
+++ cs_modules/core/idle/atalhoPublicacoesEletronicas.js
@@ -1,33 +1,34 @@
 // atalhoPublicacoesEletronicas
-function atalhoPublicacoesEletronicas (BaseName) {
+function atalhoPublicacoesEletronicas(BaseName) {
   const mconsole = new __mconsole(BaseName + '.atalhoPublicacoesEletronicas')
 
   // Verifica se o link existe.
   const url = 'publicacoes/controlador_publicacoes.php?acao=publicacao_pesquisar&id_orgao_publicacao=0'
   const txtitle = 'Publicações Eletrônicas'
-  const linkStyle = seiVersionCompare('>', '4')
-    ? getProps()
-    : $('#lnkAjuda').attr('style')
-  const containerClass = seiVersionCompare('>', '4') ? 'nav-item d-md-flex' : 'infraAcaoBarraSistema'
+  const containerClass = 'nav-item d-md-flex seipp-atalho-publicacoes-eletronicas'
 
-  $.ajax({ url: GetBaseUrl() + url }).done(function () { AdicionarAtalho() })
-    .fail(function () { mconsole.log('Página de publicações não existe') })
+  fetch(GetBaseUrl() + url).then(response => {
+    if (!response.ok) {
+      throw new Error('Página de publicações não existe')
+    }
+  }).then(() => {
+    AdicionarAtalho()
+  }).catch(e => mconsole.log(e.message))
 
   function AdicionarAtalho () {
-    $('#divInfraBarraSistemaD, #divInfraBarraSistemaPadraoD').prepend($('<div/>')
-      .addClass(containerClass)
-      .append($('<a>')
-        .attr('href', url)
-        .attr('title', txtitle)
-        .attr('style', linkStyle)
-        .attr('target', '_blank')
-        .text(txtitle)
-      )
-    )
-  }
-}
+    const a = document.createElement('a')
+    a.href = url
+    a.title = txtitle
+    a.target = '_blank'
+    a.textContent = txtitle
 
-function getProps () {
-  const style = getComputedStyle(document.querySelector('.power-off-btn, #lnkInfraSairSistema'))
-  return `align-self: center; border: none; color: ${style.color}; font-size: ${style.fontSize};`
+    const div = document.createElement('div')
+    div.className = containerClass
+    div.appendChild(a)
+
+    const barra = document.querySelector('#divInfraBarraSistemaPadraoD')
+    if (barra) {
+      barra.prepend(div)
+    }
+  }
 }
